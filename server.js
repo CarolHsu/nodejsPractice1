898591cd693e66879b71e84742dc81fd8b0f5a62
Carol Hsu <sheseee@gmail.com>
2013-04-03 14:38:25 +0800

    finished function of receiving request and response.

--- server.js
+++ server.js
@@ -3,14 +3,18 @@ var url = require("url");
 
 function start(route, handle) {
 	function onRequest(request, response){
+		var postData = "";
 		var pathname = url.parse(request.url).pathname;
 		console.log("Request for " + pathname + " received.");
-		route(handle, pathname, response);
-		// 把response在route呼叫中完成
-		// response.writeHead(200, {"Content-Type" : "text/plain"});
-		// var content = route(handle, pathname);
-		// response.write(content);
-		// response.end();
+		request.setEncoding("utf8");
+		// add request listener
+		request.addListener("data", function(postDataChunk){
+			postData += postDataChunk;
+			console.log("received post data chunk '" + postDataChunk + "' .");
+		});
+		request.addListener("end", function(){
+			route(handle, pathname, response, postData);
+		});
 	}
 
 	http.createServer(onRequest).listen(8888);
